f758682a23bea49d085fe708647603eba0379f7a
Stephen Phillips <stephen@wellthon.com>
2019-10-02 12:40:59 -0500

    adding in import

--- myFiles/helloworld/main.go
+++ myFiles/helloworld/main.go
@@ -3,10 +3,19 @@
 // go fmt- formats all the code in each file in the current directory
 // go install- compiles and installs a package
 // go get- downloads the raw source code of someone else's package
-// runs any tests associated with the current project
+// go test- runs any tests associated with the current project
 
+// package is a where all the files belong
+// main is used to make an executeable package- makes runable file
+// MUST have func called main
+
+// package calculator- defines a package that can be used as a dependency (helper code)
+// package uploader- defines a package that can be used as a dependency (helper code)
 package main
 
+// access to some code in another library
+// fmt- format package
+// main has no access to other packages outside main unless imported
 import "fmt"
 
 func main() {
